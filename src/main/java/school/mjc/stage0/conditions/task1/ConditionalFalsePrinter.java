580ce704dd2f99d772363b6a9f8ab5b8a21ddaa5
Artak1999 <dashyan.1999@mail.ru>
2022-07-05 13:47:40 +0500

    Very interesting.

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        if(a > b)
+            System.out.println(true);
+        else
+            System.out.println(false);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        if(a < b)
+            System.out.println(true);
+        else
+            System.out.println(false);
     }
 }
